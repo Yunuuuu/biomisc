8aba4b926a1c42be5f963f1ae21e255e515575f3
yun <yunyunpp96@outlook.com>
2022-08-25 15:59:03 +0800

    define global variable

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+.onAttach <- function(libname, pkgname) {
+    version <- utils::packageDescription(pkgname, fields = "Version")
+
+    packageStartupMessage(
+        "Package: ", pkgname, " (version: ", version, ")\n",
+        "Miscellaneous functions for Bioinformatics!!"
+    )
+    invisible()
+}
+
+if (getRversion() >= "2.15.1") {
+    utils::globalVariables(
+        c( # run_absolute
+            "Chromosome", "Sample", "Tumor_Sample_Barcode",
+            "group_id"
+        )
+    )
+}
