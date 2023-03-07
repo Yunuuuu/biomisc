9eeea905e35d697b2201e25c70dee639861166a1
yun <yunyunpp96@outlook.com>
2023-03-07 20:57:36 +0800

    add cytoband data

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -1,5 +1,21 @@
 ## code to prepare `internal` dataset goes here
 
+# run_arm_cnv --------------------------------------------------------
+
+# ** ref_cytoband ---------------------------------------------------------
+
+anno_hub <- AnnotationHub::AnnotationHub(localHub = TRUE)
+AnnotationHub::query(
+    anno_hub, c("UCSC", "Homo sapiens", "cytoband"),
+    ignore.case = TRUE
+)
+# AH53177 | UCSC cytoBand track for hg19
+# AH53178 | UCSC cytoBand track for hg38
+
+run_arm_cnv_ref_cytoband_hg19 <- anno_hub[["AH53177"]]
+run_arm_cnv_ref_cytoband_hg38 <- anno_hub[["AH53178"]]
+
+# run_cibersort -----------------------------------------------------------
 cibersort_lm22 <- readr::read_tsv(
     "data-raw/run_cibersort/LM22.txt",
     col_names = TRUE
@@ -24,6 +40,8 @@ absolute_disease_map_data <- ls(
 usethis::use_data(
     cibersort_lm22,
     absolute_disease_map_data,
+    run_arm_cnv_ref_cytoband_hg19,
+    run_arm_cnv_ref_cytoband_hg38,
     internal = TRUE, overwrite = TRUE,
     compress = "gzip"
 )
