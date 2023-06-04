3f37ca11c9174b326a7e64cda9fd374a7a3fb76a
yun <yunyunpp96@outlook.com>
2023-06-05 00:05:17 +0800

    fix Docs and stricter arguments checking

--- R/pyclone-utils.R
+++ R/pyclone-utils.R
@@ -1,9 +1,6 @@
 #' Prepare data for the input of PyClone or PyClone-vi
-#' @param mut_data A data.frame of mutation data with the top 4 columns (omit
-#' column specified in `sample_id`) containing "chromosome", "pos", "ref_counts"
-#' and "var_counts", other columns will be omited. Column names don't matter. If
-#' `sample_id` is not NULL, the column specified in `sample_id` will be matched
-#' in both mut_data and cnv_data.
+#'
+#' @param mut_data A data.frame of mutation data.
 #' @param cnv_data A data.frame of allele-specific CNV data with the top 5
 #' columns containing "chromosome", "start_pos", "end_pos", "major_cn", and
 #' "minor_cn". "sample_id" is optional, details see mut_data. other columns will
@@ -13,9 +10,10 @@
 #' should generally be 2 except for sex chromosomes in males.
 #' @param pyclone_vi a logical value indicates whether prepare data for the
 #' input of PyClone-vi. Details see <https://github.com/Roth-Lab/pyclone-vi>
-#' @param purity Only used for
-#' [PyClone-vi](https://github.com/Roth-Lab/pyclone-vi), the tumour content
-#' (cellularity) of the sample. Default value is 1.0 if column is not present.
+#' @param purity_field Only used for
+#' [PyClone-vi](https://github.com/Roth-Lab/pyclone-vi), a string specifying the
+#' tumour content (cellularity) column (can in `mut_data` or `cnv_data`) of the
+#' sample. Default value is 1.0 if NULL.
 #' @param error_rate Only used for
 #' [PyClone-vi](https://github.com/Roth-Lab/pyclone-vi), sequencing error rate.
 #' @references
@@ -23,69 +21,56 @@
 #'  - <https://github.com/Roth-Lab/pyclone-vi>
 #'  - <https://bitbucket.org/sequenzatools/sequenza/src/v2.1.1/R/next.R>
 #' @export
-prepare_pyclone <- function(mut_data, cnv_data, on_sample = NULL, normal_cn = 2L, pyclone_vi = FALSE, purity = 1L, error_rate = NULL) {
-    assert_class(mut_data, function(x) {
-        inherits(x, "data.frame") && ncol(x) >= 4L
-    }, "{.cls data.frame} with at least 4 columns")
-    assert_class(cnv_data, function(x) {
-        inherits(x, "data.frame") && ncol(x) >= 5L
-    }, "{.cls data.frame} with at least 5 columns")
-    if (pyclone_vi) {
-        assert_class(purity, is_scalar_numeric,
-            "scalar numeric",
-            cross_msg = NULL
-        )
-    }
-    if (!is.null(on_sample)) {
-        cnv_sample_col <- unname(on_sample)
-        mut_sample_col <- names(on_sample) %||% cnv_sample_col
-        cnv_sample_id <- cnv_data[[cnv_sample_col]]
-        cnv_data[[cnv_sample_col]] <- NULL
-        mut_sample_id <- mut_data[[mut_sample_col]]
-        mut_data[[mut_sample_col]] <- NULL
-    } else {
-        cnv_sample_id <- rep_len("sample", nrow(cnv_data))
-        mut_sample_id <- rep_len("sample", nrow(mut_data))
-    }
+prepare_pyclone <- function(
+    mut_data, cnv_data, on_sample = NULL, on_chr = "chr",
+    mut_pos = "pos", start_field = "start", end_field = "end",
+    major_cn_field = "major_cn", minor_cn_field = "minor_cn",
+    ref_counts_field = "ref_counts", var_counts_field = "var_counts",
+    purity_field = NULL, normal_cn = 2L,
+    pyclone_vi = FALSE, error_rate = NULL) {
+    assert_class(mut_data, "data.frame")
+    assert_class(cnv_data, "data.frame")
 
-    mut_data <- data.table::as.data.table(mut_data)[, 1:4]
-    data.table::setnames(
-        mut_data,
-        c("chromosome", "pos", "ref_counts", "var_counts")
-    )
-    mut_data[, sample_id := mut_sample_id] # nolint
-
-    cnv_data <- data.table::as.data.table(cnv_data)[, 1:5]
-    data.table::setnames(
-        cnv_data,
-        c("chromosome", "start_pos", "end_pos", "major_cn", "minor_cn")
-    )
+    mut_data <- data.table::as.data.table(mut_data)
+    cnv_data <- data.table::as.data.table(cnv_data)
     cnv_data[, sample_id := cnv_sample_id] # nolint
 
     out <- mut_match_cn(
         mut_data, cnv_data,
-        on_chr = "chromosome", mut_pos = "pos",
-        start_field = "start_pos", end_field = "end_pos",
-        on_sample = "sample_id"
+        on_chr = on_chr, mut_pos = mut_pos,
+        start_field = start_field, end_field = end_field,
+        on_sample = on_sample
     )
     out[, mutation_id := paste(chromosome, position, sep = ":")]
     out[, normal_cn := normal_cn]
-
-    data.table::setcolorder(
-        out,
-        c(
-            "mutation_id", "ref_counts", "var_counts", "normal_cn", "minor_cn",
-            "major_cn"
-        )
+    columns <- c(
+        "mutation_id", ref_counts_field, var_counts_field,
+        "normal_cn", c(minor_cn_field, major_cn_field)
+    )
+    column_names <- c(
+        "mutation_id", "ref_counts", "var_counts", "normal_cn", "minor_cn",
+        "major_cn"
     )
     if (pyclone_vi) {
-        # out[, sample_id := sample_id]
-        out[, tumour_content := purity]
+        assert_class(purity_field, rlang::is_scalar_character,
+            "scalar {.cls character}",
+            null_ok = TRUE
+        )
+        if (is.null(purity_field)) {
+            purity_field <- "..purity.."
+            mut_data$..purity.. <- 1L
+        }
+        columns <- c(columns, purity_field)
+        column_names <- c(column_names, "purity")
+    }
+
+    out <- out[, .SD, SDcols = c(
+        "mutation_id", ref_counts_field, var_counts_field,
+        "normal_cn", c(minor_cn_field, major_cn_field)
+    )]
+    data.table::setnames(out, column_names)
+    if (pyclone_vi) {
         data.table::setnames(out, "var_counts", "alt_counts")
-        # data.table::setcolorder(out,
-        #     "sample_id",
-        #     after = "mutation_id"
-        # )
         data.table::setcolorder(out,
             c("minor_cn", "normal_cn"),
             after = "major_cn"
@@ -104,8 +89,6 @@ prepare_pyclone <- function(mut_data, cnv_data, on_sample = NULL, normal_cn = 2L
     }
     if (!is.null(on_sample)) {
         data.table::setcolorder(out, "sample_id")
-    } else {
-        out[, sample_id := NULL] # nolint
     }
     out[major_cn > 0L]
 }
@@ -123,18 +106,12 @@ prepare_pyclone <- function(mut_data, cnv_data, on_sample = NULL, normal_cn = 2L
 #' @param start_field,end_field A string indicating the column names in
 #' `cnv_data` that contains the start positions and end position of the genomic
 #' ranges.
-#' @return A integrated
+#' @return A integrated data.frame with data column from both mut_data and
+#' cnv_data.
 #' @export
 identify_mut_cn <- function(
     mut_data, cnv_data, on_sample = NULL, on_chr = "chr",
     mut_pos = "pos", start_field = "start", end_field = "end") {
-    assert_class(on_sample, rlang::is_scalar_character,
-        "scalar character",
-        null_ok = TRUE
-    )
-    assert_class(on_chr, rlang::is_scalar_character, "scalar character")
-    assert_class(start_field, rlang::is_scalar_character, "scalar character")
-    assert_class(end_field, rlang::is_scalar_character, "scalar character")
     mut_cn <- mut_match_cn(
         mut_data = mut_data, cnv_data = cnv_data,
         on_sample = on_sample, on_chr = on_chr,
@@ -150,7 +127,51 @@ identify_mut_cn <- function(
 #' @noRd
 mut_match_cn <- function(
     mut_data, cnv_data, on_sample = NULL, on_chr = "chr",
-    mut_pos = "pos", start_field = "start", end_field = "end") {
+    mut_pos = "pos", start_field = "start", end_field = "end",
+    on_sample_arg = rlang::caller_arg(on_sample),
+    on_chr_arg = rlang::caller_arg(on_chr),
+    mut_pos_arg = rlang::caller_arg(on_chr),
+    start_field_arg = rlang::caller_arg(start_field),
+    end_field_arg = rlang::caller_arg(end_field),
+    call = parent.frame()) {
+    assert_class(on_sample, rlang::is_scalar_character,
+        "scalar {.cls character}",
+        null_ok = TRUE,
+        arg = on_sample_arg,
+        call = call
+    )
+    assert_class(on_chr, rlang::is_scalar_character,
+        "scalar {.cls character}",
+        arg = on_chr_arg,
+        call = call
+    )
+    assert_class(mut_pos, rlang::is_scalar_character,
+        "scalar {.cls character}",
+        arg = mut_pos_arg,
+        call = call
+    )
+    assert_class(start_field, rlang::is_scalar_character,
+        "scalar {.cls character}",
+        arg = start_field_arg,
+        call = call
+    )
+    assert_class(end_field, rlang::is_scalar_character,
+        "scalar {.cls character}",
+        arg = end_field_arg,
+        call = call
+    )
+    mut_column <- c(
+        names(on_sample) %||% on_sample,
+        names(on_chr) %||% on_chr,
+        mut_pos
+    )
+    assert_class(mut_data, function(x) {
+        inherits(x, "data.frame") && all(mut_column %chin% names(mut_data))
+    }, msg = "{data.frame} with column {.val {mut_column}}")
+    cnv_column <- c(on_sample, on_chr, start_field, end_field)
+    assert_class(cnv_data, function(x) {
+        inherits(x, "data.frame") && all(cnv_column %chin% names(cnv_data))
+    }, msg = "{data.frame} with column {.val {cnv_column}}")
     if (!is.null(on_sample)) {
         on_string <- paste(
             names(on_sample) %||% on_sample,
@@ -172,8 +193,8 @@ mut_match_cn <- function(
         on = on_string, nomatch = NA
     ]
     # check the match works well
-    failed_pos <- mut_cn[["pos"]] < mut_cn[["start_pos"]] |
-        mut_cn[["pos"]] > mut_cn[["end_pos"]]
+    failed_pos <- mut_cn[[mut_pos]] < mut_cn[[start_field]] |
+        mut_cn[[mut_pos]] > mut_cn[[end_field]]
     if (any(failed_pos)) {
         cli::cli_abort("Something wrong when parsing CN of mutation")
     }
