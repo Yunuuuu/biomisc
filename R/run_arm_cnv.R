1b2f94c01d8a7d6196ef442fdb7f8c6b4d1773c8
yun <yunyunpp96@outlook.com>
2023-06-21 18:15:50 +0800

    use summarize_arm to impelement

--- R/run_arm_cnv.R
+++ R/run_arm_cnv.R
@@ -1,35 +1,22 @@
 #' Caculate Chromosome-arm-levels copy number variation
 #'
-#' @param seg_cnv A [data.frame][data.frame] obeject with CNV values in
+#' @inheritParams summarize_arm
+#' @param seg_data A [data.frame][data.frame] obeject with CNV values in
 #'   "cnv_field" column (absolute segment copy number determined by ABSOLUTE
 #'   algorithm, which is the sum of allelic copy numbers, or relative segment
 #'   copy number defined with -1 meaning del, 0 meaning neutral and 1 meaning
 #'   amp).
-#' @param sample_field A string indicates the sample Id column in seg_cnv.
 #' @param cnv_field A scalar character gives the column containing CNV values.
-#' @param chr_field,start_field,end_field A string specifying the column of the
-#'  chromosome name, start positions and end positions of the genomic ranges in
-#'  seg_cnv.
-#' @param ref_cytoband A scalar string `"hg19"` or `"hg38"`, in this way, see
-#'   [get_cytoband], or you can provided a self-defined
-#'   [GenomicRanges][GenomicRanges::GenomicRanges] obeject containing the
-#'   Cytoband reference.
-#' @inheritParams get_arm_ranges
 #' @param cnv_mode One of "rel" and "abs" correspongding to what Shukla, A and
 #'   Cohen-Sharir have presented respectively.
-#' @param ... Not used currently.
-#' @param filter_centromere Whether to include or exclude segments across
-#'   centromere, namely genomic ranges interseted with "acen" arm of
-#'   ref_cytoband.  Default: `TRUE`.
 #' @param threshold The fraction to define Chromosome arm-level aneuploidy
-#'   profiling, Shukla, A. used 0.9 as the cut-off ("rel" cnv_mode). If NULL,
-#'   the fraction of aberrant genome will be returned.
+#'   profiling, Shukla, A. used 0.9 as the cut-off ("rel" cnv_mode).
 #' @param ploidy_field A single numeric indicates the ploidy to define
 #'   Chromosome arm-level aneuploidy profiling.  Cohen-Sharir uses background
 #'   ploidy ("abs" cnv_mode) derived from [run_absolute][ABSOLUTE::RunAbsolute]
 #'   algorithm. Or you can also provide a string to specifying the ploidy column
-#'   in `seg_cnv`. If NULL, the weighted median by aberrant genome width will be
-#'   returned.
+#'   in `seg_cnv`.
+#' @inheritDotParams summarize_arm -seg_data -sample_field -other_fields
 #' @author Yun \email{yunyunpp96@@outlook.com}
 #' @return A [data.table][data.table::data.table] containing
 #' Chromosome-arm-levels copy number.
@@ -43,11 +30,8 @@
 #' }
 #' @export
 run_arm_cnv <- function(
-    seg_cnv, sample_field = NULL, cnv_field, ploidy_field = 2L,
-    chr_field = "chr", start_field = "startpos", end_field = "endpos",
-    ref_cytoband = "hg38", arm_field = NULL, arms = c("p", "q"),
-    cnv_mode = c("rel", "abs"), ...,
-    filter_centromere = TRUE, threshold = 0.9) {
+    seg_data, sample_field = NULL, cnv_field, cnv_mode = c("rel", "abs"),
+    ploidy_field = 2L, threshold = 0.9, ...) {
     assert_pkg("GenomeInfoDb")
     assert_pkg("matrixStats")
     cnv_mode <- match.arg(cnv_mode)
@@ -60,7 +44,6 @@ run_arm_cnv <- function(
         cross_msg = NULL
     )
     other_fields <- cnv_field
-    group_fields <- sample_field
     if (cnv_mode == "abs") {
         assert_class(ploidy_field,
             function(x) {
@@ -69,76 +52,37 @@ run_arm_cnv <- function(
             cross_msg = NULL, null_ok = TRUE
         )
         if (is.character(ploidy_field)) {
-            assert_nest(seg_cnv, ploidy_field, group = sample_field)
-            group_fields <- c(sample_field, ploidy_field)
+            assert_nest(seg_data, ploidy_field, group = sample_field)
+            other_fields <- c(cnv_field, ploidy_field)
         }
+    } else {
+        assert_class(
+            threshold,
+            is_scalar_numeric, "scalar {.cls numeric}",
+            cross_msg = NULL
+        )
     }
-    seg_cnv <- prepare_granges(
-        data = seg_cnv,
-        chr_field = chr_field,
-        start_field = start_field,
-        end_field = end_field,
-        other_fields = c(other_fields, group_fields),
-        keep.extra.columns = TRUE,
-        ignore.strand = TRUE
+    out <- summarize_arm(
+        seg_data = seg_data,
+        sample_field = sample_field,
+        other_fields = other_fields,
+        ...
     )
-    assert_range_unique(seg_cnv, group = sample_field)
-    cnv_values <- S4Vectors::mcols(seg_cnv)[[cnv_field]]
-    if (!is.numeric(cnv_values)) {
+    data.table::setnames(out, cnv_field, "CNV")
+    if (!is.numeric(out$CNV)) {
         cli::cli_abort("CNV values must be numeric")
     }
-
     if (cnv_mode == "abs") {
-        if (!all(cnv_values >= 0L)) {
+        if (!all(out$CNV >= 0L)) {
             cli::cli_abort("Copy number values must be positive or 0L in {.val {cnv_mode}} CNV mode")
         }
     } else {
-        S4Vectors::mcols(seg_cnv)[[cnv_field]] <- data.table::fcase(
-            cnv_values < 0L, -1L, cnv_values > 0L, 1L,
+        out$CNV <- data.table::fcase(
+            out$CNV < 0L, -1L, out$CNV > 0L, 1L,
             default = 0L
         )
     }
-    if (rlang::is_scalar_character(ref_cytoband) &&
-        any(ref_cytoband == c("hg19", "hg38"))) {
-        ref_cytoband <- get_cytoband(ref_cytoband, add_arm = TRUE)
-        arm_field <- "arm"
-    } else if (!inherits(ref_cytoband, "GenomicRanges")) {
-        cli::cli_abort(
-            '{.arg ref_cytoband} must be a scalar character ("hg19" or "hg38"), or a self-defined {.cls GenomicRanges} object.'
-        )
-    }
-    cytoband_seqstyle <- GenomeInfoDb::seqlevelsStyle(ref_cytoband)
-    seg_cnv <- map_seqnames(seg_cnv, cytoband_seqstyle)
-
-    arm_cytoband <- get_arm_ranges(ref_cytoband,
-        arm_field = arm_field, arms = unique(c(arms, "acen"))
-    )
-    if (filter_centromere) {
-        acen_region <- arm_cytoband[
-            S4Vectors::mcols(arm_cytoband)[[arm_field]] == "acen"
-        ]
-        # Remove any segment that sligthly overlaps the centromere
-        centromere_hits <- silent_expr(
-            GenomicRanges::findOverlaps(seg_cnv, acen_region),
-            "Each of the 2 combined objects has sequence levels not in the other:",
-            fixed = TRUE
-        )
-        if (length(centromere_hits) > 0) {
-            seg_cnv <- seg_cnv[-S4Vectors::queryHits(centromere_hits)]
-        }
-    }
-    arm_cytoband <- arm_cytoband[
-        S4Vectors::mcols(arm_cytoband)[[arm_field]] %in% arms
-    ]
-    out <- seg_to_arm_seg(
-        seg_data = seg_cnv,
-        arm_cytoband = arm_cytoband,
-        arm_field = arm_field,
-        group_fields = group_fields,
-        other_fields = cnv_field
-    )
-    data.table::setnames(out, cnv_field, "CNV")
-    if (cnv_mode == "abs" && !is.null(ploidy_field)) {
+    if (cnv_mode == "abs") {
         if (is.character(ploidy_field)) {
             data.table::setnames(out, ploidy_field, "ploidy")
         } else {
@@ -156,24 +100,13 @@ run_arm_cnv <- function(
             },
             by = c(sample_field, "chr", "arm")
         ]
-    } else if (cnv_mode == "abs") {
-        out[,
-            list(arm_cnv = matrixStats::weightedMedian(
-                    CNV, w = width, na.rm = TRUE # nolint styler: off
-            )),
-            by = c(sample_field, "chr", "arm")
-        ]
-    } else if (cnv_mode == "rel" && !is.null(threshold)) {
+    } else {
         out[,
             list(
                 arm_cnv = as.integer(
                     sum(abs(CNV) * width / arm_width, na.rm = TRUE) > threshold
                 )
             ),
-            by = c(sample_field, "chr", "arm")
-        ]
-    } else {
-        out[, list(arm_cnv = CNV * width / arm_width),
             by = c(sample_field, "chr", "arm")
         ]
     }
