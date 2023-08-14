87a032f908cbbe3ce7bd1547ca681495cb0713a8
yun <yunyunpp96@outlook.com>
2023-08-15 02:25:51 +0800

    enable customazition of index_fn

--- R/nmf_consensus.R
+++ R/nmf_consensus.R
@@ -26,8 +26,9 @@
 #' (across all tumor) to be considered significant.
 #' @param founder_intersection_size Minimal size of significant intersected
 #' programs to define the first NMF program in a cluster.
-#' @param child_intersection_size Minimal intersection cutoff for adding a new
+#' @param min_overlap_index Minimal intersection cutoff for adding a new
 #' NMF program to the forming cluster.
+#' @param index_fn Function to define overlap index between two gene sets.
 #' @references
 #' Gavish, A., Tyler, M., Greenwald, A.C. et al. Hallmarks of transcriptional
 #' intratumour heterogeneity across a thousand tumours. Nature 618, 598–606
@@ -35,7 +36,7 @@
 #' @seealso
 #' <https://github.com/tiroshlab/3ca>
 #' @export
-nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02, min_size = 3L, min_intra_sim = 0.7, min_intra_size = NULL, min_inter_sim = 0.2, min_inter_size = 2L, redundant_sim = 0.2, min_intersection = 0.2, founder_intersection_size = 0.2, child_intersection_size = 0.2) {
+nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02, min_size = 3L, min_intra_sim = 0.7, min_intra_size = NULL, min_inter_sim = 0.2, min_inter_size = 2L, redundant_sim = 0.2, min_intersection = 0.2, founder_intersection_size = 0.2, min_overlap_index = 0.2, index_fn = jaccard_index) {
     assert_class(
         basis_list, function(list) {
             is.list(list) && all(vapply(list, function(sublist) {
@@ -45,12 +46,13 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
         }, "{.cls list} of sublist (length >= 2) of {.cls matrix}",
         cross_msg = NULL
     )
+    assert_class(index_fn, is.function, "{.cls function}", cross_msg = NULL)
     # Modified from <https://github.com/tiroshlab/3ca/blob/main/ITH_hallmarks/Generating_MPs/Generate_Meta_Programs.R>
     ### Parameters for clustering
     # min_intersection: the minimal intersection cutoff for defining the
     # first NMF program in a cluster;
 
-    # child_intersection_size: the minimal intersection cutoff for adding a new
+    # min_overlap_index: the minimal intersection cutoff for adding a new
     # NMF to the forming cluster;
 
     # founder_intersection_size: the minimal group size to consider for defining
@@ -81,7 +83,9 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
             list()
         } else {
             intra_similarity <- sapply(intra_program_list, function(x) {
-                sapply(intra_program_list, jaccard_index, x = x)
+                sapply(intra_program_list, function(y) {
+                    index_fn(x, y)
+                })
             })
             intra_program_list[
                 rowSums(intra_similarity >= min_intra_sim) - 1L >=
@@ -102,7 +106,7 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
         inter_counts <- vapply(curr_program, function(program) {
             sum(vapply(others, function(other) {
                 any(vapply(other, function(y) {
-                    jaccard_index(program, y)
+                    index_fn(program, y)
                 }, numeric(1L)) >= min_inter_sim)
             }, numeric(1L)))
         }, numeric(1L))
@@ -117,7 +121,7 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
                 idx <- c(idx, i)
             } else {
                 intra_similarity <- vapply(idx, function(i2) {
-                    jaccard_index(curr_program[[i]], curr_program[[i2]])
+                    index_fn(curr_program[[i]], curr_program[[i2]])
                 }, numeric(1L))
                 if (max(intra_similarity) >= redundant_sim) {
                     next
@@ -133,29 +137,19 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
     nmf_programs <- unlist(recur_program_list,
         recursive = FALSE, use.names = TRUE
     )
+
     # define consensus modules ---------------------------------
     # calculate similarity between programs
     nmf_intersect <- sapply(nmf_programs, function(x) {
         sapply(nmf_programs, function(y) length(intersect(x, y)))
     })
+    nmf_union <- sapply(nmf_programs, function(x) {
+        sapply(nmf_programs, function(y) length(union(x, y)))
+    })
+    sim_matrix <- nmf_intersect / nmf_union
     min_length <- sapply(nmf_programs, function(x) {
         sapply(nmf_programs, function(y) min(lengths(list(x, y))))
     })
-    # nmf_union <- sapply(nmf_programs, function(x) {
-    #     sapply(nmf_programs, function(y) length(union(x, y)))
-    # })
-    # hierarchical clustering of the similarity matrix
-    # nmf_intersect_hc <- stats::hclust(
-    #     stats::as.dist(1 - nmf_intersect / nmf_union),
-    #     method = "average"
-    # )
-    # nmf_intersect_hc <- stats::reorder(
-    #     stats::as.dendrogram(nmf_intersect_hc),
-    #     colMeans(nmf_intersect)
-    # )
-    # order_hc <- stats::order.dendrogram(nmf_intersect_hc)
-    # nmf_intersect <- nmf_intersect[order_hc, order_hc]
-    # min_length <- min_length[order_hc, order_hc]
     if (min_intersection > 0L && min_intersection < 1L) {
         min_intersection <- min_length * min_intersection
     }
@@ -187,30 +181,21 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
             setdiff(names(nmf_programs), cur_program_id)
         ] # remove selected NMF
 
-        intersection_with_genes_mp <- nmf_intersect[
-            names(nmf_programs), cur_program_id,
-            drop = TRUE
-        ]
-        intersection_with_genes_mp <- sort(
-            intersection_with_genes_mp,
-            decreasing = TRUE
-        ) # intersection between all other NMFs and genes_mp
+        ovaerlap_genes_mp <- sapply(nmf_programs, function(y) {
+            index_fn(genes_mp, y)
+        })
+        # intersection between all other NMFs and genes_mp
+        ovaerlap_genes_mp <- sort(ovaerlap_genes_mp, decreasing = TRUE)
         all_genes_mp <- genes_mp
         # has genes in all NMFs in the current cluster, for redefining genes_mp
         # after adding a new NMF
-        ### Create gene list is composed of intersecting genes (in descending order by frequency). When the number of genes with a given frequency span bewond the 50th genes, they are sorted according to their NMF score. 
+        ### Create gene list is composed of intersecting genes (in descending order by frequency). When the number of genes with a given frequency span bewond the 50th genes, they are sorted according to their NMF score.
 
-        if (child_intersection_size > 0L && child_intersection_size < 1L) {
-            child_intersection_threshold <- module_size * child_intersection_size
-        } else {
-            child_intersection_threshold <- child_intersection_size
-        }
-        child_intersection_threshold <- as.integer(child_intersection_threshold)
-        while (intersection_with_genes_mp[1] >= child_intersection_threshold) {
-            next_program_id <- names(intersection_with_genes_mp[1])
+        while (ovaerlap_genes_mp[1] >= min_overlap_index) {
+            next_program_id <- names(ovaerlap_genes_mp[1])
             cur_program <- c(cur_program, next_program_id)
-
-            all_genes_mp <- c(all_genes_mp, nmf_programs[[next_program_id]])
+            next_gene_mp <- nmf_programs[[next_program_id]]
+            all_genes_mp <- c(all_genes_mp, next_gene_mp)
             all_genes_mp_counts <- sort(table(all_genes_mp), decreasing = TRUE)
             # genes_mp is newly defined each time according to all NMFs in the
             # current cluster
@@ -229,10 +214,10 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
                 for (i in cur_program) {
                     curr_study <- strsplit(i, ".", fixed = TRUE)[[1]]
                     curr_study <- as.integer(curr_study)
-                    module_basis <- basis_list[[curr_study[1:2]]]
-                    curr_nmf_coef <- module_basis[
+                    curr_basis <- basis_list[[curr_study[1:2]]]
+                    curr_nmf_coef <- curr_basis[
                         intersect(
-                            names(genes_at_border), rownames(module_basis)
+                            names(genes_at_border), rownames(curr_basis)
                         ),
                         curr_study[[3L]],
                         drop = TRUE
@@ -241,7 +226,7 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
                 }
                 nmf_coef <- sort(nmf_coef, decreasing = TRUE)
                 nmf_coef <- nmf_coef[unique(names(nmf_coef))]
-                all_genes_mp_counts <- c(
+                genes_mp <- c(
                     names(all_genes_mp_counts[
                         all_genes_mp_counts > all_genes_mp_counts[
                             min(module_size, length(all_genes_mp_counts))
@@ -250,41 +235,19 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
                     names(nmf_coef)
                 )
             } else {
-                all_genes_mp_counts <- names(all_genes_mp_counts)[
-                    seq_len(min(module_size, length(all_genes_mp_counts)))
-                ]
+                genes_mp <- names(all_genes_mp_counts)
             }
-            genes_mp <- all_genes_mp_counts[
-                seq_len(min(module_size, length(all_genes_mp_counts)))
-            ]
+            genes_mp <- genes_mp[seq_len(min(module_size, length(genes_mp)))]
             remain_programs <- setdiff(names(nmf_programs), next_program_id)
             if (length(remain_programs) == 0L) break
             nmf_programs <- nmf_programs[remain_programs] # remove selected NMF
-            intersection_with_genes_mp <- nmf_intersect[
-                names(nmf_programs), next_program_id,
-                drop = TRUE
-            ]
-            names(intersection_with_genes_mp) <- names(nmf_programs)
-            intersection_with_genes_mp <- sort(
-                intersection_with_genes_mp,
-                decreasing = TRUE
-            ) # intersection between all other NMFs and genes_mp
+            ovaerlap_genes_mp <- sapply(nmf_programs, function(y) {
+                index_fn(genes_mp, y)
+            })
+            # intersection between all other NMFs and genes_mp
+            ovaerlap_genes_mp <- sort(ovaerlap_genes_mp, decreasing = TRUE)
         }
-        program_list[[paste0("MP", k)]] <- vapply(
-            strsplit(cur_program, ".", fixed = TRUE), function(x) {
-                idx <- as.integer(x)
-                first_id <- (names(basis_list) %||% seq_along(basis_list))[
-                    idx[[1L]]
-                ]
-                basis <- basis_list[[idx[[1L]]]]
-                second_id <- (names(basis) %||% seq_along(basis))[idx[2L]]
-                cur_basis <- basis[[idx[2]]]
-                third_id <- (colnames(cur_basis) %||% seq_len(ncol(cur_basis)))[
-                    idx[[3L]]
-                ]
-                paste(first_id, second_id, third_id, sep = "-")
-            }, character(1L)
-        )
+        program_list[[paste0("MP", k)]] <- cur_program
         MP_list[[paste0("MP", k)]] <- genes_mp
         if (!is.null(min_intersection) && all(dim(min_intersection) == dim(nmf_intersect))) {
             min_intersection <- min_intersection[
@@ -306,31 +269,81 @@ nmf_consensus <- function(basis_list, module_size = 50L, min_contribution = 0.02
         cur_program <- NULL
         k <- k + 1
     }
+
+    # order final program data based on clustering output --------
+    # hierarchical clustering of the similarity matrix
+    nmf_hc <- stats::hclust(stats::as.dist(1 - sim_matrix), method = "average")
+    nmf_hc <- stats::reorder(stats::as.dendrogram(nmf_hc), colMeans(sim_matrix))
+    order_hc <- stats::order.dendrogram(nmf_hc)
+    sim_matrix <- sim_matrix[order_hc, order_hc]
+    idxs_sorted <- unlist(program_list, recursive = FALSE, use.names = FALSE)
+    sim_matrix_group <- rep(names(program_list), times = lengths(program_list))
+    non_classified <- setdiff(colnames(sim_matrix), idxs_sorted)
+    idxs_sorted <- c(idxs_sorted, non_classified)
+    sim_matrix_group <- c(
+        sim_matrix_group,
+        rep_len("none", length(non_classified))
+    )
+    sim_matrix <- sim_matrix[idxs_sorted, idxs_sorted]
+
     recur_program_list <- mapply(
         function(programs, basis) {
             if (length(programs) == 0L) {
                 return(list())
             }
-            idx <- strsplit(names(programs), ".", fixed = TRUE)
-            idx <- data.table::transpose(idx) # rank_id, factor_id
-            rank_ids <- names(basis) %||% seq_along(basis)
-            names(programs) <- mapply(function(x, y) {
-                x <- as.integer(x)
-                y <- as.integer(y)
-                rank_id <- rank_ids[[x]]
-                cur_basis <- basis[[x]]
-                factor_ids <- colnames(cur_basis) %||% seq_len(ncol(cur_basis))
-                factor_id <- factor_ids[[y]]
-                paste(rank_id, factor_id, sep = "-")
-            }, x = idx[[1L]], y = idx[[2L]], USE.NAMES = FALSE)
+            names(programs) <- nmf_consensus_parse_name(
+                names(programs),
+                basis = basis
+            )
             programs
         },
         basis = basis_list,
         programs = recur_program_list,
         SIMPLIFY = FALSE, USE.NAMES = TRUE
     )
+    program_list <- lapply(program_list, function(programs) {
+        nmf_consensus_parse_name(programs, basis_list)
+    })
+    rownames(sim_matrix) <- nmf_consensus_parse_name(
+        rownames(sim_matrix), basis_list
+    )
+    colnames(sim_matrix) <- nmf_consensus_parse_name(
+        colnames(sim_matrix), basis_list
+    )
     list(
         recur_program_list = recur_program_list,
-        meta_program = MP_list, clusters = program_list
+        meta_program = MP_list, clusters = program_list,
+        sim_matrix = sim_matrix, sim_matrix_group = sim_matrix_group
     )
+}
+
+nmf_consensus_parse_name <- function(idx_string, basis_list = NULL, basis = NULL) {
+    vapply(strsplit(idx_string, ".", fixed = TRUE), function(x) {
+        if (is.null(basis_list)) {
+            all_names <- nmf_consensus_parse_name_rank(as.integer(x), basis)
+        } else {
+            all_names <- nmf_consensus_parse_name_project(
+                as.integer(x), basis_list
+            )
+        }
+        paste(all_names, collapse = ".")
+    }, character(1L))
+}
+
+nmf_consensus_parse_name_project <- function(idx, basis_list) {
+    project_id <- (names(basis_list) %||% seq_along(basis_list))[
+        idx[[1L]]
+    ]
+    basis <- basis_list[[idx[[1L]]]]
+    c(project_id, nmf_consensus_parse_name_rank(idx[-1L], basis))
+}
+
+nmf_consensus_parse_name_rank <- function(idx, basis) {
+    rank_id <- (names(basis) %||% seq_along(basis))[idx[1L]]
+    curr_basis <- basis[[idx[[1L]]]]
+    c(rank_id, nmf_consensus_parse_name_factor(idx[[2L]], curr_basis))
+}
+
+nmf_consensus_parse_name_factor <- function(idx, basis) {
+    (colnames(basis) %||% seq_len(ncol(basis)))[idx]
 }
