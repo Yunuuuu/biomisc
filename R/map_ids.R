0e1bcbffdbd3168b7700edb20a46121874ab879c
yun <yunyunpp96@outlook.com>
2023-09-26 23:39:27 +0800

    enhance map_ids function

--- R/map_ids.R
+++ R/map_ids.R
@@ -21,9 +21,10 @@ methods::setGeneric(
 #' a `OrgDb` or `ChipDb` object.
 #' @param column The columns or kinds of things that can be retrieved from the
 #' database.
-#' @param keytype The keytype that matches the keys (`rownames(x)`) used.
+#' @param keytype The keytype that matches the keys (`rownames(x)` or column
+#' specified in `swap_rownames`) used.
 #' @param decreasing A bool, Should the sort order be increasing or decreasing?
-#' @rdname map_ids
+#' @name map_ids
 #' @export
 methods::setMethod("map_ids", "ANY", function(x, db, column, keytype, decreasing = TRUE) {
     feature_data <- map_ids_internal(
@@ -37,40 +38,70 @@ methods::setMethod("map_ids", "ANY", function(x, db, column, keytype, decreasing
     x
 })
 
-#' @param swap_rownames String or integer specifying the
-#' [rowData][SummarizedExperiment::rowData] column containing the features
-#' names. If `NULL`, `rownames(x)` will be used. 
-#' @param assay_name A string or integer scalar indicating which
-#' [assay][SummarizedExperiment::assay] in the `x` to calculate the mean value
-#' for each row. IF `NULL`, the first assay will be used.
 #' @rdname map_ids
 #' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @export
 methods::setMethod("map_ids", "SummarizedExperiment", function(x, db, column, keytype, swap_rownames = NULL, assay_name = NULL, decreasing = TRUE) {
+    out <- map_ids_for_s4(
+        x = x, db = db, column = column, keytype = keytype,
+        decreasing = decreasing, swap_rownames = swap_rownames,
+        rowdata = SummarizedExperiment::rowData(x),
+        assay = SummarizedExperiment::assay(x, i = assay_name %||% 1L)
+    )
+    SummarizedExperiment::rowData(out$x) <- cbind(
+        x$feature_data, SummarizedExperiment::rowData(out$x)
+    )
+    out$x
+})
+
+#' @rdname map_ids
+#' @importClassesFrom Biobase ExpressionSet
+#' @export
+methods::setMethod("map_ids", "ExpressionSet", function(x, db, column, keytype, swap_rownames = NULL, assay_name = NULL, decreasing = TRUE) {
+    out <- map_ids_for_s4(
+        x = x, db = db, column = column, keytype = keytype,
+        decreasing = decreasing, swap_rownames = swap_rownames,
+        rowdata = Biobase::fData(x),
+        assay = Biobase::assayData(x)[[assay_name %||% "exprs"]]
+    )
+    Biobase::fData(out$x) <- cbind(x$feature_data, Biobase::fData(out$x))
+    out$x
+})
+
+#' @param swap_rownames String or integer specifying the
+#' [rowData][SummarizedExperiment::rowData] or [fData][Biobase::fData] column
+#' containing the features names. If `NULL`, `rownames(x)` will be used.
+#' @param assay_name A string or integer scalar indicating which
+#' [assay][SummarizedExperiment::assay] or [assayData][Biobase::assayData] in
+#' the `x` to calculate the mean value for each row. IF `NULL`, the 1st assay
+#' ([SummarizedExperiment][SummarizedExperiment::SummarizedExperiment]) or
+#' "exprs" ([fData][Biobase::ExpressionSet]) will be used.
+#' @name map_ids
+NULL
+
+map_ids_for_s4 <- function(x, db, column, keytype, decreasing, swap_rownames, rowdata, assay, call = rlang::caller_call()) {
     assert_(swap_rownames, function(x) {
         length(x) == 1L && (is.character(x) || is.numeric(x))
-    }, "a string or integer", null_ok = TRUE)
+    }, "a string or integer", null_ok = TRUE, call = call)
     if (is.null(swap_rownames)) {
         id <- rownames(x)
     } else {
-        id <- SummarizedExperiment::rowData(x)[[swap_rownames]]
+        swap_rownames <- .use_names_to_integer_indices(
+            swap_rownames, colnames(rowdata)
+        )
+        id <- rowdata[[swap_rownames]]
     }
     feature_data <- map_ids_internal(
         id = id, db = db,
         column = column, keytype = keytype, decreasing = decreasing,
-        order_by = rowMeans(
-            SummarizedExperiment::assay(x, i = assay_name %||% 1L),
-            na.rm = TRUE
-        )
+        order_by = rowMeans(assay, na.rm = TRUE),
+        call = call
     )
     x <- x[feature_data$idx, ]
     feature_data <- feature_data[c(keytype, column)]
     rownames(x) <- feature_data[[column]]
-    SummarizedExperiment::rowData(x) <- cbind(
-        feature_data, SummarizedExperiment::rowData(x)
-    )
-    x
-})
+    list(x = x, feature_data = feature_data)
+}
 
 map_ids_internal <- function(id, db, column, keytype, order_by, decreasing = TRUE, call = rlang::caller_env()) {
     assert_pkg("AnnotationDbi", fun = "map_ids", call = call)
@@ -88,7 +119,7 @@ map_ids_internal <- function(id, db, column, keytype, order_by, decreasing = TRU
     out <- eval(data.table::substitute2(
         out[.id != "" & !is.na(.id)][ # nolint
             order(val, decreasing = decreasing)
-        ], 
+        ],
         list(.id = column)
     ))
     out <- unique(out, by = column, cols = c("idx", keytype))
