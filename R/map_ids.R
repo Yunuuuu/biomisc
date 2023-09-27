0ab880b15336329fc4b60024422571788ec36b6c
yun <yunyunpp96@outlook.com>
2023-09-27 17:30:03 +0800

    enable mapping in a named db

--- R/map_ids.R
+++ R/map_ids.R
@@ -8,7 +8,7 @@
 #' all samples.
 #' @param x A matrix-like entity, such as a
 #' [SummarizedExperiment][SummarizedExperiment::SummarizedExperiment] or an
-#' [ExpressionSet][Biobase::ExpressionSet] object. 
+#' [ExpressionSet][Biobase::ExpressionSet] object.
 #' @param ... Other arguments passed to specific methods.
 #' @name map_ids
 #' @export
@@ -20,31 +20,42 @@ methods::setGeneric(
 
 #' @param db An [AnnotationDb][AnnotationDbi::AnnotationDb] object. But in
 #' practice this will mean an object derived from an AnnotationDb object such as
-#' a `OrgDb` or `ChipDb` object.
+#' a `OrgDb` or `ChipDb` object. Alternatively, a named atomic vector can be
+#' used to match `keytype` by names.
 #' @param column The columns or kinds of things that can be retrieved from the
-#' database.
+#' `db` database.
 #' @param keytype The keytype that matches the keys (`rownames(x)` or column
 #' specified in `swap_rownames`) used.
 #' @param decreasing A bool, Should the sort order be increasing or decreasing?
 #' @rdname map_ids
 #' @export
 methods::setMethod("map_ids", "ANY", function(x, db, column, keytype, decreasing = TRUE) {
-    feature_data <- map_ids_internal(
-        id = rownames(x), db = db,
+    out <- map_ids_internal(
+        x = x, db = db,
         column = column, keytype = keytype,
         decreasing = decreasing,
-        order_by_matrix = x
+        assay = x
     )
-    x <- x[feature_data$idx, , drop = FALSE]
-    rownames(x) <- feature_data[[column]]
-    x
+    out$x
 })
+
+
+#' @param swap_rownames String or integer specifying the
+#' [rowData][SummarizedExperiment::rowData] or [fData][Biobase::fData] column
+#' containing the features names. If `NULL`, `rownames(x)` will be used.
+#' @param assay_name A string or integer scalar indicating which assay in the
+#' `x` to calculate the mean value for each row. If `NULL`, the `1st` assay (if
+#' x is a [SummarizedExperiment][SummarizedExperiment::SummarizedExperiment]
+#' object) or [exprs][Biobase::exprs] (if x is an
+#' [ExpressionSet][Biobase::ExpressionSet] object) will be used.
+#' @name map_ids
+NULL
 
 #' @rdname map_ids
 #' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @export
 methods::setMethod("map_ids", "SummarizedExperiment", function(x, db, column, keytype, swap_rownames = NULL, assay_name = NULL, decreasing = TRUE) {
-    out <- map_ids_swap(
+    out <- map_ids_internal(
         x = x, db = db, column = column, keytype = keytype,
         decreasing = decreasing, swap_rownames = swap_rownames,
         rowdata = SummarizedExperiment::rowData(x),
@@ -60,7 +71,7 @@ methods::setMethod("map_ids", "SummarizedExperiment", function(x, db, column, ke
 #' @importClassesFrom Biobase ExpressionSet
 #' @export
 methods::setMethod("map_ids", "ExpressionSet", function(x, db, column, keytype, swap_rownames = NULL, assay_name = NULL, decreasing = TRUE) {
-    out <- map_ids_swap(
+    out <- map_ids_internal(
         x = x, db = db, column = column, keytype = keytype,
         decreasing = decreasing, swap_rownames = swap_rownames,
         rowdata = Biobase::fData(x),
@@ -70,62 +81,66 @@ methods::setMethod("map_ids", "ExpressionSet", function(x, db, column, keytype, 
     out$x
 })
 
-#' @param swap_rownames String or integer specifying the
-#' [rowData][SummarizedExperiment::rowData] or [fData][Biobase::fData] column
-#' containing the features names. If `NULL`, `rownames(x)` will be used.
-#' @param assay_name A string or integer scalar indicating which assay in the
-#' `x` to calculate the mean value for each row. If `NULL`, the `1st` assay (if
-#' x is a [SummarizedExperiment][SummarizedExperiment::SummarizedExperiment]
-#' object) or [exprs][Biobase::exprs] (if x is an
-#' [ExpressionSet][Biobase::ExpressionSet] object) will be used.
-#' @name map_ids
-NULL
-
-map_ids_swap <- function(x, db, column, keytype, decreasing, swap_rownames, rowdata, assay, call = rlang::caller_call()) {
-    assert_(swap_rownames, function(x) {
-        length(x) == 1L && (is.character(x) || is.numeric(x))
-    }, "a string or integer", null_ok = TRUE, call = call)
-    if (is.null(swap_rownames)) {
-        id <- rownames(x)
-    } else {
-        swap_rownames <- use_names_to_integer_indices(
-            swap_rownames, colnames(rowdata)
-        )
-        id <- rowdata[[swap_rownames]]
-    }
-    feature_data <- map_ids_internal(
-        id = id, db = db,
-        column = column, keytype = keytype, decreasing = decreasing,
-        order_by_matrix = assay,
-        call = call
-    )
-    x <- x[feature_data$idx, ]
-    feature_data <- feature_data[c(keytype, column)]
-    rownames(x) <- feature_data[[column]]
-    list(x = x, feature_data = feature_data)
-}
-
-map_ids_internal <- function(id, db, column, keytype, order_by_matrix, decreasing = TRUE, call = rlang::caller_env()) {
+map_ids_internal <- function(x, db, column, keytype, decreasing, assay, swap_rownames = NULL, rowdata, call = rlang::caller_call()) {
     assert_pkg("AnnotationDbi", fun = "map_ids", call = call)
     assert_string(keytype, empty_ok = FALSE, call = call)
     assert_string(column, empty_ok = FALSE, call = call)
-    out <- eval(data.table::substitute2(
+    assert_bool(decreasing, call = call)
+    assert_(swap_rownames, function(x) {
+        length(x) == 1L && (is.character(x) || is.numeric(x))
+    }, "a string or integer", null_ok = TRUE, call = call)
+
+    # swap rownames ------------------------
+    if (is.null(swap_rownames)) {
+        id <- rownames(x)
+        if (is.null(id)) {
+            rlang::abort(sprintf("no rownames found in %s", format_arg(x)))
+        }
+    } else {
+        swap_rownames <- use_names_to_integer_indices(
+            swap_rownames, colnames(rowdata),
+            call = call
+        )
+        id <- rowdata[[swap_rownames]]
+    }
+
+    # map ids -------------------------------
+    feature_data <- eval(data.table::substitute2(
         data.table::data.table(name = id), list(name = keytype)
     ))
-    out$idx <- seq_along(id)
-    out[[column]] <- AnnotationDbi::mapIds(
-        x = db, keys = out[[keytype]], column = column,
-        keytype = keytype
-    )
-    out$val <- rowMeans(order_by_matrix, na.rm = TRUE)
-    out <- eval(data.table::substitute2(
-        out[.id != "" & !is.na(.id)][ # nolint
-            order(val, decreasing = decreasing)
-        ],
+    feature_data$idx <- seq_along(id)
+    if (rlang::is_atomic(db)) {
+        if (rlang::is_named(db)) {
+            feature_data[[column]] <- db[feature_data[[keytype]]]
+        } else {
+            rlang::abort(
+                sprintf("atomic %s must be named", format_arg("db")),
+                call = call
+            )
+        }
+    } else {
+        feature_data[[column]] <- AnnotationDbi::mapIds(
+            x = db, keys = feature_data[[keytype]], column = column,
+            keytype = keytype
+        )
+    }
+
+    # filter duplicates ------------------------------
+    feature_data$.val <- rowMeans(assay, na.rm = TRUE)
+    feature_data <- eval(data.table::substitute2(
+        feature_data[.id != "" & !is.na(.id)], # nolint,
         list(.id = column)
     ))
-    out <- unique(out, by = column, cols = c("idx", keytype))
-    data.table::setcolorder(out, c("idx", keytype, column))
-    data.table::setDF(out, rownames = out[[column]])
+    if (decreasing) {
+        data.table::setorderv(feature_data, c(column, ".val"), c(1L, -1L))
+    } else {
+        data.table::setorderv(feature_data, c(column, ".val"), c(1L, 1L))
+    }
+    feature_data <- unique(feature_data, by = column)
+    x <- x[feature_data$idx, , drop = FALSE]
+    feature_data <- feature_data[, .SD, .SDcols = c(keytype, column)]
+    data.table::setDF(feature_data, rownames = feature_data[[column]])
+    list(x = x, feature_data = feature_data)
 }
-utils::globalVariables(c("val", ".id"))
+
+utils::globalVariables(".id")
